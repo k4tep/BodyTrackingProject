9f095eaaf6fa666e25d55e1d6f6a19304b19f40c
kate <katepgoesintothesunset@gmail.ru>
2024-05-10 17:26:55 +0300

    feat: add closing popUp on 'X' button

--- components/popup/popup.tsx
+++ components/popup/popup.tsx
@@ -4,8 +4,12 @@ import MyInput from '../input/input';
 import styles from './popup.module.scss';
 import dayjs from 'dayjs';
 import { postData } from '@/api/data/post-data';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 export default function MyPopUp(props: { setPopUp: any; popUp: boolean }) {
+    library.add(fas);
     const [newData, setNewData] = useState({
         date: dayjs(Date.now()),
         value: 0,
@@ -29,12 +33,15 @@ export default function MyPopUp(props: { setPopUp: any; popUp: boolean }) {
     }
 
     return (
-        <div
-            className={props.popUp ? styles.popup_container : styles.popup_container_none}
-            onClick={() => {
-                props.setPopUp(false);
-            }}
-        >
+        <div className={props.popUp ? styles.popup_container : styles.popup_container_none}>
+            <FontAwesomeIcon
+                icon={['fas', 'circle-xmark']}
+                size="2xl"
+                className={styles.close_btn}
+                onClick={() => {
+                    props.setPopUp(false);
+                }}
+            />
             <div
                 className={styles.popup_input_container}
                 onClick={(event) => {
@@ -54,7 +61,7 @@ export default function MyPopUp(props: { setPopUp: any; popUp: boolean }) {
                     }
                 />
                 <button
-                    className={styles.auth_btn}
+                    className={styles.save_btn}
                     onClick={() => {
                         postNewData();
                     }}
